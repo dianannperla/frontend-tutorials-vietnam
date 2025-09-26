e198448a420a378225d8d0a1965c03df6f5c0f73
dianannperla <waafirmelyza@hotmail.com>
2025-09-26 12:50:17 +0100

    Add a simple function to alert the user when the application loads

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Frontend Tutorials Vietnam application is now loaded');
+    alert('Welcome to the Frontend Tutorials Vietnam application!');
     // Additional JavaScript functionality can be added here
 });
