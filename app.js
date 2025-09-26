037bb68dfe6877c8909b7dbeec2959c322642af1
dianannperla <waafirmelyza@hotmail.com>
2025-09-26 12:50:03 +0100

    Update console log message for clarity

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log('Frontend Tutorials Vietnam Loaded');
+    console.log('Frontend Tutorials Vietnam application is now loaded');
     // Additional JavaScript functionality can be added here
 });
